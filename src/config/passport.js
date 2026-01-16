9f4e84dc391220a56124efc2bf4c2bd47828f95f
unknown <shubhashisbaral@gmail.com>
2026-01-16 12:46:41 +0530

    addeed config of passport o auth

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,8 +8,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
-    },
+     callbackURL: process.env.GOOGLE_CALLBACK_URL,
+},
     async (accessToken, refreshToken, profile, done) => {
       try {
         const email = profile.emails?.[0]?.value;
